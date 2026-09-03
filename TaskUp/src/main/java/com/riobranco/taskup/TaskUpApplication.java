1add5a58c6aa8149620a7ddb3dbc26d845691a16
beatrizlod12 <bealopesdias@gmail.com>
2026-09-03 20:40:47 -0300

    Setup inicial do projeto Spring Boot

---
+++ TaskUp/src/main/java/com/riobranco/taskup/TaskUpApplication.java
@@ -0,0 +1,13 @@
+package com.riobranco.taskup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskUpApplication.class, args);
+	}
+
+}
---
+++ TaskUp/src/test/java/com/riobranco/taskup/TaskUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riobranco.taskup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
