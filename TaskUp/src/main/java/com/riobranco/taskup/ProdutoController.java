285fd6896cb2d298fc2c361a5d272784bea1de75
beatrizlod12 <bealopesdias@gmail.com>
2026-09-25 18:48:56 -0300

    Configuração de CORS

--- TaskUp/src/main/java/com/riobranco/taskup/ProdutoController.java
+++ TaskUp/src/main/java/com/riobranco/taskup/ProdutoController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
@@ -49,7 +47,7 @@ public class ProdutoController {
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
-    
+
     // DELETE
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletar(@PathVariable Long id) {
